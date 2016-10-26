e2f1e3c547808559492b5be53559391a83e6c3a4
mr.PP <mr.pp@localhost.localdomain>
2016-10-27 00:11:46 +0200

    add solution for predictOnevsAll

--- machine-learning-ex3/ex3/predictOneVsAll.m
+++ machine-learning-ex3/ex3/predictOneVsAll.m
@@ -28,7 +28,13 @@ X = [ones(m, 1) X];
 %       max element, for more information see 'help max'. If your examples 
 %       are in rows, then, you can use max(A, [], 2) to obtain the max 
 %       for each row.
-%       
+%
+result_matrix = sigmoid(all_theta * X');
+%disp(result_matrix(:,1))
+[temp, p] = max(result_matrix);
+%disp(p);
+%disp(temp);
+        
 
 
 
